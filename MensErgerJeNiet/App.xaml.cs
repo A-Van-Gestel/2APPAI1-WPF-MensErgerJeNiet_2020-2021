bf8d31a8e7bddf3e6b36491fb815f8fb8c7da210
Axel Van Gestel <r0784084@student.thomasmore.be>
2021-03-05 19:58:34 +0100

    Added: Splash Screen

--- MensErgerJeNiet/App.xaml.cs
+++ MensErgerJeNiet/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +14,25 @@ namespace MensErgerJeNiet
     /// </summary>
     public partial class App : Application
     {
+        private const int SplashTime = 1500; // Miliseconds
+
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+            // Create new SplashScreen & show it without autoclosing
+            SplashScreen splash = new SplashScreen("images/SplashScreen.png");
+            splash.Show(false);
+
+            // Create new MainWindow
+            MainWindow main = new MainWindow();
+
+            // Sleep to show SplashScreen longer
+            Thread.Sleep(SplashTime);
+
+            // Close SplashScreen with a fade
+            splash.Close(TimeSpan.FromMilliseconds(300));
+
+            // Show MainWindow
+            main.Show();
+        }
     }
 }
